3d2512bde75948b9fc610b8ffbb1a705d81a590a
DESKTOP_THEO\theod <theodore.lecointe@lecole-ldlc.com>
2023-05-23 11:11:06 +0200

    client Mysql

---
+++ configMysql.php
@@ -0,0 +1,20 @@
+<?php
+
+const MYSQL_HOST = 'http://10.96.16.1/';
+const MYSQL_PORT = 8080;
+const MYSQL_NAME = 'pokemon';
+const MYSQL_USER = 'root';
+const MYSQL_PASSWORD = '';
+
+try {
+    $mysqlClient = new PDO(
+        sprintf('mysql:host=%s;dbname=%s;port=%s', MYSQL_HOST, MYSQL_NAME, MYSQL_PORT),
+        MYSQL_USER,
+        MYSQL_PASSWORD
+    );
+    $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(Exception $exception) {
+    die('Erreur : '.$exception->getMessage());
+}
+
+<?
