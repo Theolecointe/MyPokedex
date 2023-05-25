244ac2e1a494eab81ca3b8e3e52c247304c9bbb4
LAPTOP-HL6GFI7F\tom-e <tom-eliott.ferrand@lecole-ldlc.com>
2023-05-25 11:42:52 +0200

    header modif, en attente de deux balises php dans page connexion et page deconexion

--- header.php
+++ header.php
@@ -33,6 +33,42 @@
   <header>
     <img src="pokedex2.png" alt="Logo Pokemon" class="header-image">
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     
+     <?php if (isset($_SESSION['utilisateur_connecte'])) { ?>
+
+      <style>
+    /* Styles CSS pour l'en-tête */
+    header {
+      background-color: #2a75bb;
+      padding: 20px;
+      display: flex;
+      align-items: center;
+    }
+
+    .header-box {
+      margin-right: 17px;
+    }
+
+    .header-box button {
+      background: none;
+      border: none;
+      font-size: 16px;
+      color: #333;
+      cursor: pointer;
+    }
+
+    .header-image {
+      margin-right: 20px;
+      width: 100px;
+    }
+  </style>
+      
+      <div class="header-box"> 
+      <button onclick="window.location.href = 'page-info.html';">Mes Pokemons</button>
+    </div>
+    <div class="header-box">
+      <button onclick="window.location.href = 'page-pokedex.html';">Mes combats</button>
+    </div>
     <div class="header-box">
       <button onclick="window.location.href = 'page-connexion.html';">Pokedex</button>
     </div>
@@ -42,6 +78,20 @@
     <div class="header-box">
       <button onclick="window.location.href = 'page-pokedex.html';">Connexion</button>
     </div>
+
+
+    <?php } else { ?>
+      <div class="header-box">
+        <button onclick="window.location.href = 'page-info-non-connecte.php';">Info</button>
+      </div>
+      <div class="header-box">
+        <button onclick="window.location.href = 'page-connexion.php';">Connexion</button>
+      </div>
+      <div class="header-box">
+        <button onclick="window.location.href = 'page-inscription.php';">Inscription</button>
+      </div>
+
+    <?php } ?>
   </header>
 </body>
 </html>
