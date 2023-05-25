94594945a0896ce4dd5ff22ed7181b6faafa6c35
DESKTOP_THEO\theod <theodore.lecointe@lecole-ldlc.com>
2023-05-25 11:17:55 +0200

    Version Fonctionnelle

--- submitcreateaccount.php
+++ submitcreateaccount.php
@@ -8,18 +8,39 @@ if (isset($_POST["nom"]) && isset($_POST["password"]))
 
 	$nom = $_POST["nom"];
 	$password = $_POST["password"];
-	$checkexistdresseur = $mysqlClient ->prepare('SELECT * FROM dresseur WHERE nom = '$nom''); 
-	$checkexistdresseur -> execute(); 
+	$checkexistdresseur = $mysqlClient ->prepare('SELECT nom FROM dresseur WHERE nom = :nom'); 
+	$checkexistdresseur -> execute([  
+
+		 'nom' => $nom,
+	]); 
 
 	$responsecheckloggin = $checkexistdresseur->fetch(PDO::FETCH_ASSOC);
-	echo($responsecheckloggin);
 
-	echo($responsecheckloggin);
+	if ($responsecheckloggin == True) {
+
+		echo('Ce nom d\'utilisateur existe déja, choisi en un autre');
+	}
+
+	else{
+
+		$includeutilisateur = $mysqlClient ->prepare('INSERT INTO dresseur (nom, password) VALUES (:nom, :password)'); 
+		$includeutilisateur -> execute([  
+
+		 'nom' => $nom,
+		 'password' => $password,
+
+		]); 
+
+		if($includeutilisateur){
+
+			echo ("Nouveau Compte Créer avec Succes");
+		}
+	}
 
 }
 else
 {
-	echo("not set");
+	echo("Il faut donner un Nom et un Mot de passe pour créer un compte");
 }
 
 
