a1c1402a72c7969bb0a67fa6c0473b7741859a26
LAPTOP-VH97HQU7\leo-p <leo-paul.grimaldi@lecole-ldlc.com>
2023-05-30 10:24:00 +0200

    info v1

---
+++ info.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+<body>
+  
+</body>
+</html>
+
+<?php
+echo "Notre site et un pokedex virtuel qui vous pernet de collectionner une mulltitude de pokemon et de voir tout le pokemon que vous avez capturé";
+
