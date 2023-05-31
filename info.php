df6c486aac0acd33aca5f8700cbfb83b8ccdc9a0
LAPTOP-HL6GFI7F\tom-e <tom-eliott.ferrand@lecole-ldlc.com>
2023-05-31 14:49:54 +0200

    last fix

--- info.php
+++ info.php
@@ -2,15 +2,68 @@
 
 <!DOCTYPE html>
 <html>
-
 <head>
-
+  <title>Pokédex - Info</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 20px;
+    }
+    
+    h1 {
+      color: #ff0000;
+    }
+    
+    p {
+      margin-bottom: 10px;
+    }
+  </style>
 </head>
 <body>
+  <h1>Pokédex - Info</h1>
   
+  <h2>Pourquoi ce site a-t-il été créé ?</h2>
+  <p>Le site MyPokédex a été créé dans le but de fournir aux dresseurs de Pokémon une ressource complète et détaillée sur les différents Pokémon de l'univers de Pokémon. Il vise à rassembler toutes les informations essentielles sur chaque Pokémon, y compris leur type, leurs statistiques, leurs évolutions, et bien plus encore. Ce site est destiné à faciliter la recherche et la compréhension des dresseurs lors de leurs aventures.</p>
+  
+  <h2>Pour qui est ce site ?</h2>
+  <p>Ce site est destiné aux dresseurs de Pokémon de tous niveaux d'expérience. Que vous soyez un débutant qui souhaite en apprendre d'avantage sur les Pokémon et leurs caractéristiques, ou un dresseur chevronné à la recherche de détails spécifiques sur des Pokémon particuliers, ce site vous offre une source d'informations précieuse. Les joueurs de tous âges et de tous horizons peuvent utiliser ce site pour améliorer leurs connaissances sur les Pokémon.</p>
+  
+  <h2>Que pouvez-vous trouver sur ce site ?</h2>
+  <p>Sur ce site, vous pouvez trouver des informations détaillées sur les différents Pokémon, notamment :</p>
+  <ul>
+    <li>Le type de chaque Pokémon</li>
+    <li>Leur description et leur histoire</li>
+    <li>Leurs statistiques telles que les points de vie, l'attaque et la défense</li>
+    <li>Leurs évolutions et les conditions nécessaires pour les obtenir</li>
+    <li>Quel dresseur possède ce pokemon</li>
+    <li>Des images et des illustrations des Pokémon</li>
+  </ul>
+  
+  <p>Que vous souhaitiez en apprendre davantage sur votre Pokémon préféré ou que vous recherchiez des informations stratégiques pour votre équipe de combat, ce site vous fournira les informations dont vous avez besoin.</p>
+  <?php include_once('footer/footer.php'); ?>
+<style>
+html,
+body {
+    height: 100%;
+    margin: 0;
+}
+
+.page-wrapper {
+    min-height: 100%;
+    position: relative;
+}
+
+.content-wrapper {
+    padding-bottom: 2.5rem; /* Hauteur du pied de page */
+}
+
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 2.5rem; /* Hauteur du pied de page */
+}
+</style>
 </body>
 </html>
-
-<?php
-echo "Notre site et un pokedex virtuel qui vous pernet de collectionner une mulltitude de pokemon et de voir tout le pokemon que vous avez capturé";
 
