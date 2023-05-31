d9079724aff71ddfec474b029a15f374c0c1fa4a
DESKTOP_THEO\theod <theodore.lecointe@lecole-ldlc.com>
2023-05-31 15:07:46 +0200

    Dernière de la dernière des dernières versions

--- retrivePokemon.php
+++ retrivePokemon.php
@@ -1,11 +1,12 @@
 <?php 
 
 
+
 include_once('configMysql.php');
 
 $nom_dresseur = $_SESSION['LOGGED_USER'];
 
-	$retrivePokemon = $mysqlClient ->prepare('SELECT pokemon.nom, dresseur.nb_combats, region.nom as region_nom, type.nom as type FROM pokemon JOIN pokemons_existants ON pokemons_existants.id_Pokemon = pokemon.id JOIN dresseur ON dresseur.id = pokemons_existants.id_dresseur JOIN region ON pokemon.id_region = region.id JOIN est_de_type ON pokemon.id = est_de_type.id_pkmn JOIN type ON est_de_type.id_Type = type.id WHERE dresseur.nom = :nom_dresseur');
+	$retrivePokemon = $mysqlClient ->prepare('SELECT pokemon.nom, dresseur.nb_combats, region.nom as region_nom, type.nom as type, pokemons_existants.id FROM pokemon JOIN pokemons_existants ON pokemons_existants.id_Pokemon = pokemon.id JOIN dresseur ON dresseur.id = pokemons_existants.id_dresseur JOIN region ON pokemon.id_region = region.id JOIN est_de_type ON pokemon.id = est_de_type.id_pkmn JOIN type ON est_de_type.id_Type = type.id WHERE dresseur.nom = :nom_dresseur GROUP BY pokemons_existants.id');
 
 	$retrivePokemon -> execute([
 
@@ -15,5 +16,7 @@ $nom_dresseur = $_SESSION['LOGGED_USER'];
 	
 
 	$responseRetrivePokemon = $retrivePokemon->fetchAll(PDO::FETCH_ASSOC);
+
+	//print_r($responseRetrivePokemon);
 
 ?>
